c50dde43e2866b28a524dd1ff6382160f6545e9b
Ben <isieldor@gmail.com>
2022-03-02 11:28:05 -0600

    added custom colors (need implementing)

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      dbackground: "#36393f",
+      dbackgrounddark: "#2f3136",
+      dbackgrounddarkest: "#202225",
+      dblurple: "#5865f2",
+      dtext: "dcddde",
+      dtextinput: "#40444b",
+    },
     extend: {},
   },
   plugins: [],
